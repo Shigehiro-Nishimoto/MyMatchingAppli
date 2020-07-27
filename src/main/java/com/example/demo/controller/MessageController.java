73cf77b8d5d6ce910484878692a35ced59013bf7
hayato fujimura <h-fujimura@rough_and_ready.co.jp>
2020-07-27 18:01:58 +0900

    さらに、メッセージ画面での機能作成中に終了

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.Message;
 import com.example.demo.login.domain.service.UserService;
 
 @Controller
@@ -12,10 +15,15 @@ public class MessageController {
 
 	@Autowired
 	UserService userService;
-	
+
 	//メッセージ画面のGETメソッド
 	@GetMapping("/message")
 	public String getMessage(Model model) {
 		return "login/message";
+	}
+	
+	@PostMapping(value = "/message")
+	public String MessageWirtten(@ModelAttribute("message") Message form, Model model) {
+	return "redirect:/message";
 	}
 }
---
+++ src/main/java/com/example/demo/login/domain/model/Message.java
@@ -0,0 +1,12 @@
+package com.example.demo.login.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+
+	private String written;
+
+}
